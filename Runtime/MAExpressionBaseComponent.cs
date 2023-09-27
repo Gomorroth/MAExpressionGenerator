eb9406bc8dd9057bee73dc198e8bfcf1584d2b1c
Ashley Scarlet <70315656+AshleyScarlet@users.noreply.github.com>
2023-09-27 20:17:43 +0900

    mofu

--- Runtime/MAExpressionBaseComponent.cs
+++ Runtime/MAExpressionBaseComponent.cs
@@ -3,21 +3,12 @@ using VRC.SDKBase;
 
 namespace gomoru.su.ModularAvatarExpressionGenerator
 {
-    [DefaultExecutionOrder(-10000)]
     [ExecuteInEditMode]
     public abstract class MAExpressionBaseComponent : MonoBehaviour, IEditorOnly
     {
         protected internal const string InitialParameterPrefix = "\u200B";
 
 #if UNITY_EDITOR
-        private void Awake()
-        {
-            if (UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
-            {
-                RuntimeUtils.OnAwake?.Invoke(this);
-            }
-        }
-
         private void Update()
         {
             if (_flag && !UnityEditor.EditorApplication.isPlayingOrWillChangePlaymode)
